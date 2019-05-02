5684505a710c50d2183a9bd080af123486d6a1a3
Cal-D <blackpinetree92@gmail.com>
2019-05-02 18:28:23 +0900

    다른 분의 코드를 사용, 훨씬 간단하게 가능하다.기억하자 codility 2-2

--- src/practice/Algorithm_practice16.java
+++ src/practice/Algorithm_practice16.java
@@ -1,37 +1,22 @@
 package practice;
 
-
-
 public class Algorithm_practice16 {
-	public int[] solution(int[] A, int K) {
-  		int index = 0;
-	int[] result = new int[A.length];
-	int [] empty = new int [0];
-	
-	if(A.length == 0) return empty;
-	if (K > A.length) {
-		index = K % A.length;
-	}
-	index = K;
-	
-	for (int i = 0; i < A.length; i++) {
-		if (i + index >= A.length) {
-			result[(i + index) - A.length ] = A[i];
-		}
-		else {
-			result[i + index] = A[i];
-		}
-	}
-	for(int num : result) {
-	System.out.println(num);
-	}
-	return result;
+	public int[] solution(int[] A, int K) { 
+		int arraySize = A.length;
+		int result[] = new int[arraySize];
+
+    for (int i = 0; i < arraySize; i++) {
+        result[(i + K) % arraySize] = A[i];
+    }
+    return result;
+
+
 	}
 
-public static void main(String[] args) {
-	int a[] = {1000};
-	Algorithm_practice16 aa =  new Algorithm_practice16();
-	aa.solution(a, 5);
+	public static void main(String[] args) {
+		int a[] = { 1000 };
+		Algorithm_practice16 aa = new Algorithm_practice16();
+		aa.solution(a, 5);
 
-}
+	}
 }
